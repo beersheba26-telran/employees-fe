fc90bce5e9a3e51693e611f8bff1bfc03d672f2e
Yuragranovsky <yuragranovsky@gmail.com>
2026-03-03 14:37:24 +0200

    employees table with initial responsive considerations

--- src/components/Employees.tsx
+++ src/components/Employees.tsx
@@ -8,21 +8,21 @@ const Employees = () => {
       {isLoading && <Spinner></Spinner>}
       <Stack justifyContent={"center"} alignItems={"center"} height={"100%"}>
         <Table.ScrollArea borderWidth="1px" rounded="md" height="75vh" width={{base:"95vw", md: "80vw"}}>
-          <Table.Root size="lg" stickyHeader>
+          <Table.Root size={{base: "sm", sm: "md", lg: "lg"}} stickyHeader>
             <Table.Header>
               <Table.Row bg="bg.subtle">
-                <Table.ColumnHeader></Table.ColumnHeader>
+                <Table.ColumnHeader hideBelow={"sm"}></Table.ColumnHeader>
                 <Table.ColumnHeader>Full Name</Table.ColumnHeader>
                 <Table.ColumnHeader>Department</Table.ColumnHeader>
                 <Table.ColumnHeader>Salary</Table.ColumnHeader>
-                <Table.ColumnHeader>Birthdate</Table.ColumnHeader>
+                <Table.ColumnHeader hideBelow={"sm"}>Birthdate</Table.ColumnHeader>
               </Table.Row>
             </Table.Header>
             <Table.Body>
               {employees.map((empl) => (
                 <Table.Row key={empl.id}>
-                  <Table.Cell>
-                    <Avatar.Root size={"lg"}>
+                  <Table.Cell hideBelow={"sm"} >
+                    <Avatar.Root size={{sm:"sm", lg: "lg"}} >
                       <Avatar.Fallback name={empl.fullName} />
                       <Avatar.Image src={empl.avatar} />
                     </Avatar.Root>
@@ -30,7 +30,7 @@ const Employees = () => {
                   <Table.Cell>{empl.fullName}</Table.Cell>
                   <Table.Cell >{empl.department}</Table.Cell>
                   <Table.Cell >{empl.salary}</Table.Cell>
-                  <Table.Cell >{empl.birthdate}</Table.Cell>
+                  <Table.Cell hideBelow={"sm"} >{empl.birthdate}</Table.Cell>
                 </Table.Row>
               ))}
             </Table.Body>
