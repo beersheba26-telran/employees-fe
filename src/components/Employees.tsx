825bed850619eca383c3fbdeee0ea77d86ba8e42
Yuragranovsky <yuragranovsky@gmail.com>
2026-03-03 14:22:11 +0200

    adds employees table with no responsive considerations yet

--- src/components/Employees.tsx
+++ src/components/Employees.tsx
@@ -1,17 +1,44 @@
-import { Spinner } from "@chakra-ui/react";
+import { Avatar, Spinner, Stack, Table } from "@chakra-ui/react";
 import useEmployees from "../services/hooks/useEmployees";
 
 const Employees = () => {
-  const {employees, isLoading} = useEmployees();
+  const { employees, isLoading } = useEmployees();
   return (
     <>
-    {isLoading && <Spinner></Spinner>}
-    <ul>
-      {employees.map(empl => <li key={empl.id}>{JSON.stringify(empl)}</li>)}
-    </ul>
-      
+      {isLoading && <Spinner></Spinner>}
+      <Stack justifyContent={"center"} alignItems={"center"} height={"100%"}>
+        <Table.ScrollArea borderWidth="1px" rounded="md" height="75vh" width={{base:"95vw", md: "80vw"}}>
+          <Table.Root size="lg" stickyHeader>
+            <Table.Header>
+              <Table.Row bg="bg.subtle">
+                <Table.ColumnHeader></Table.ColumnHeader>
+                <Table.ColumnHeader>Full Name</Table.ColumnHeader>
+                <Table.ColumnHeader>Department</Table.ColumnHeader>
+                <Table.ColumnHeader>Salary</Table.ColumnHeader>
+                <Table.ColumnHeader>Birthdate</Table.ColumnHeader>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {employees.map((empl) => (
+                <Table.Row key={empl.id}>
+                  <Table.Cell>
+                    <Avatar.Root size={"lg"}>
+                      <Avatar.Fallback name={empl.fullName} />
+                      <Avatar.Image src={empl.avatar} />
+                    </Avatar.Root>
+                  </Table.Cell>
+                  <Table.Cell>{empl.fullName}</Table.Cell>
+                  <Table.Cell >{empl.department}</Table.Cell>
+                  <Table.Cell >{empl.salary}</Table.Cell>
+                  <Table.Cell >{empl.birthdate}</Table.Cell>
+                </Table.Row>
+              ))}
+            </Table.Body>
+          </Table.Root>
+        </Table.ScrollArea>
+      </Stack>
     </>
-  )
-}
+  );
+};
 
-export default Employees
+export default Employees;
