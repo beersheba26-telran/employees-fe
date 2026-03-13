cd084310d05c04cedf6297a54d512b8857990d88
Yuragranovsky <yuragranovsky@gmail.com>
2026-03-13 14:48:23 +0200

    adds cell with employee details button

--- src/components/Employees.tsx
+++ src/components/Employees.tsx
@@ -1,125 +1,174 @@
 import { Avatar, IconButton, Spinner, Stack, Table } from "@chakra-ui/react";
 import { FC, ReactNode, useMemo } from "react";
 import { Employee } from "../models/Employee";
-import {FaSort, FaSortUp, FaSortDown} from "react-icons/fa"
-import { Order, SortByFieldsStore, SortField, useSortByFields } from "../state-management/sort-store";
-import orderBy from "lodash/orderBy"
+import { FaSort, FaSortUp, FaSortDown } from "react-icons/fa";
+import {
+  Order,
+  SortByFieldsStore,
+  SortField,
+  useSortByFields,
+} from "../state-management/sort-store";
+import orderBy from "lodash/orderBy";
 import { useUserData } from "../state-management/auth-store";
 
 import EditEmployee from "./EditEmployee";
 import DeleteEmployee from "./DeleteEmployee";
-
+import EmployeeDetails from "./EmployeeDetails";
 type Props = {
-  employees: Employee[]
-  isLoading: boolean
-}
-function updateSortingState(field: SortField, sortOptions: SortByFieldsStore):void {
-    const order = sortOptions[field] ;
-    let newOrder: Order = "asc"
-    if (order != "no") {
-      newOrder = order == "asc" ? "desc" : "asc";
-    }
-    sortOptions.setOrder(field, newOrder)
+  employees: Employee[];
+  isLoading: boolean;
+};
+function updateSortingState(
+  field: SortField,
+  sortOptions: SortByFieldsStore,
+): void {
+  const order = sortOptions[field];
+  let newOrder: Order = "asc";
+  if (order != "no") {
+    newOrder = order == "asc" ? "desc" : "asc";
+  }
+  sortOptions.setOrder(field, newOrder);
 }
 function getIcon(field: SortField, sortOptions: SortByFieldsStore): ReactNode {
-  const order = sortOptions[field] ;
-  let result: ReactNode = <FaSort></FaSort>
+  const order = sortOptions[field];
+  let result: ReactNode = <FaSort></FaSort>;
   if (order != "no") {
-    result = order == "asc" ? <FaSortUp> </FaSortUp> : <FaSortDown></FaSortDown>
+    result =
+      order == "asc" ? <FaSortUp> </FaSortUp> : <FaSortDown></FaSortDown>;
   }
-  return<IconButton size="xs" marginLeft={2} onClick={() => updateSortingState(field, sortOptions)}>{ result}</IconButton>;
+  return (
+    <IconButton
+      size="xs"
+      marginLeft={2}
+      onClick={() => updateSortingState(field, sortOptions)}
+    >
+      {result}
+    </IconButton>
+  );
 }
-function getSortingFields(sortOptions:SortByFieldsStore): SortField[] {
-      const keys: SortField[] = Object.keys(sortOptions) as SortField[]
-      return keys.filter(k => sortOptions[k] == "asc" || sortOptions[k] == "desc")
+function getSortingFields(sortOptions: SortByFieldsStore): SortField[] {
+  const keys: SortField[] = Object.keys(sortOptions) as SortField[];
+  return keys.filter(
+    (k) => sortOptions[k] == "asc" || sortOptions[k] == "desc",
+  );
 }
-const Employees: FC<Props> = ({employees, isLoading}) => {
-  const role = useUserData(s => s.role)
+const Employees: FC<Props> = ({ employees, isLoading }) => {
+  const role = useUserData((s) => s.role);
   const sortOptions = useSortByFields();
-  const sortedEmployees: Employee[] = useMemo(()=>{
+  const sortedEmployees: Employee[] = useMemo(() => {
     const sortFields: SortField[] = getSortingFields(sortOptions);
-    let result: Employee[] = []
-    result = sortFields.length == 0 ? orderBy(employees, ["id"], ["asc"]) : orderBy(employees, sortFields,
-      sortFields.map(sf => (sortOptions as any)[sf])
-     )
+    let result: Employee[] = [];
+    result =
+      sortFields.length == 0
+        ? orderBy(employees, ["id"], ["asc"])
+        : orderBy(
+            employees,
+            sortFields,
+            sortFields.map((sf) => (sortOptions as any)[sf]),
+          );
     return result;
-  }, [employees, sortOptions] )
+  }, [employees, sortOptions]);
   return (
     <>
       {isLoading && <Spinner></Spinner>}
       <Stack justifyContent={"center"} alignItems={"center"} height={"100%"}>
-        <Table.ScrollArea borderWidth="1px" rounded="md" height="70vh"
-         width={{base:"95vw", md: "80vw"}}>
-          <Table.Root size={{base: "sm", sm: "md", lg: "lg"}} stickyHeader
-          css={{
-          "& [data-sticky]": {
-            position: "sticky",
-            zIndex: 1,
-            bg: "bg",
+        <Table.ScrollArea
+          borderWidth="1px"
+          rounded="md"
+          height="70vh"
+          width={{ base: "95vw", md: "80vw" }}
+        >
+          <Table.Root
+            size={{ base: "sm", sm: "md", lg: "lg" }}
+            stickyHeader
+            css={{
+              "& [data-sticky]": {
+                position: "sticky",
+                zIndex: 1,
+                bg: "bg",
 
-            _after: {
-              content: '""',
-              position: "absolute",
-              pointerEvents: "none",
-              top: "0",
-              bottom: "-1px",
-              width: "32px",
-            },
-          },
+                _after: {
+                  content: '""',
+                  position: "absolute",
+                  pointerEvents: "none",
+                  top: "0",
+                  bottom: "-1px",
+                  width: "32px",
+                },
+              },
 
-          "& [data-sticky=end]": {
-            _after: {
-              insetInlineEnd: "0",
-              translate: "100% 0",
-              shadow: "inset 8px 0px 8px -8px rgba(0, 0, 0, 0.16)",
-            },
-          },
+              "& [data-sticky=end]": {
+                _after: {
+                  insetInlineEnd: "0",
+                  translate: "100% 0",
+                  shadow: "inset 8px 0px 8px -8px rgba(0, 0, 0, 0.16)",
+                },
+              },
 
-          "& [data-sticky=start]": {
-            _after: {
-              insetInlineStart: "0",
-              translate: "-100% 0",
-              shadow: "inset -8px 0px 8px -8px rgba(0, 0, 0, 0.16)",
-            },
-          },
+              "& [data-sticky=start]": {
+                _after: {
+                  insetInlineStart: "0",
+                  translate: "-100% 0",
+                  shadow: "inset -8px 0px 8px -8px rgba(0, 0, 0, 0.16)",
+                },
+              },
 
-          "& thead tr": {
-            shadow: "0 1px 0 0 {colors.border}",
-            "&:has(th[data-sticky])": {
-              zIndex: 2,
-            },
-          },
-        }}>
+              "& thead tr": {
+                shadow: "0 1px 0 0 {colors.border}",
+                "&:has(th[data-sticky])": {
+                  zIndex: 2,
+                },
+              },
+            }}
+          >
             <Table.Header>
               <Table.Row bg="bg.subtle">
-                <Table.ColumnHeader ></Table.ColumnHeader>
-                <Table.ColumnHeader  data-sticky="end" minW="160px" left="16">Full Name {getIcon("fullName", sortOptions)}</Table.ColumnHeader>
-                <Table.ColumnHeader>Department{getIcon("department", sortOptions)}</Table.ColumnHeader>
-                <Table.ColumnHeader>Salary{getIcon("salary", sortOptions)}</Table.ColumnHeader>
-                <Table.ColumnHeader >Birthdate{getIcon("birthdate", sortOptions)}</Table.ColumnHeader>
-                {role == "ADMIN" && <Table.ColumnHeader ></Table.ColumnHeader>}
-                 {role == "ADMIN" && <Table.ColumnHeader ></Table.ColumnHeader>}
+                <Table.ColumnHeader></Table.ColumnHeader>
+                <Table.ColumnHeader data-sticky="end" minW="160px" left="16">
+                  Full Name {getIcon("fullName", sortOptions)}
+                </Table.ColumnHeader>
+                <Table.ColumnHeader>
+                  Department{getIcon("department", sortOptions)}
+                </Table.ColumnHeader>
+                <Table.ColumnHeader>
+                  Salary{getIcon("salary", sortOptions)}
+                </Table.ColumnHeader>
+                <Table.ColumnHeader>
+                  Birthdate{getIcon("birthdate", sortOptions)}
+                </Table.ColumnHeader>
+                {role == "ADMIN" && <Table.ColumnHeader></Table.ColumnHeader>}
+                {role == "ADMIN" && <Table.ColumnHeader></Table.ColumnHeader>}
+                <Table.ColumnHeader></Table.ColumnHeader>
               </Table.Row>
             </Table.Header>
             <Table.Body>
               {sortedEmployees.map((empl) => (
-                <Table.Row key={empl.id} >
-                  <Table.Cell data-sticky="end" left="0" >
-                    <Avatar.Root size={{sm:"sm", lg: "lg"}} >
+                <Table.Row key={empl.id}>
+                  <Table.Cell data-sticky="end" left="0">
+                    <Avatar.Root size={{ sm: "sm", lg: "lg" }}>
                       <Avatar.Fallback name={empl.fullName} />
                       <Avatar.Image src={empl.avatar} />
                     </Avatar.Root>
                   </Table.Cell>
-                  <Table.Cell data-sticky="end" left="16">{empl.fullName}</Table.Cell>
-                  <Table.Cell >{empl.department}</Table.Cell>
-                  <Table.Cell >{empl.salary}</Table.Cell>
-                  <Table.Cell >{empl.birthdate}</Table.Cell>
-                   { role === "ADMIN" && <Table.Cell >
-                      <DeleteEmployee empl={empl}/>
-                      </Table.Cell>}
-                       { role === "ADMIN" && <Table.Cell >
-                       <EditEmployee employee={empl}/>
-                      </Table.Cell>}
+                  <Table.Cell data-sticky="end" left="16">
+                    {empl.fullName}
+                  </Table.Cell>
+                  <Table.Cell>{empl.department}</Table.Cell>
+                  <Table.Cell>{empl.salary}</Table.Cell>
+                  <Table.Cell>{empl.birthdate}</Table.Cell>
+                  {role === "ADMIN" && (
+                    <Table.Cell>
+                      <DeleteEmployee empl={empl} />
+                    </Table.Cell>
+                  )}
+                  {role === "ADMIN" && (
+                    <Table.Cell>
+                      <EditEmployee employee={empl} />
+                    </Table.Cell>
+                  )}
+                  <Table.Cell>
+                    <EmployeeDetails employee={empl} />
+                  </Table.Cell>
                 </Table.Row>
               ))}
             </Table.Body>
